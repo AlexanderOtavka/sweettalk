b2d0a54194354de54a817d15b72ac176d6b23842
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2019-07-07 14:13:28 -0400

    Add tests to loadFeatures

---
+++ src/lib/loadFeatures.spec.ts
@@ -0,0 +1,38 @@
+import test from "ava"
+import loadFeatures from "./loadFeatures"
+
+const requireFeature = (fileName: string) => {
+  if (fileName === "/features/foo") {
+    return { a: 1, b: 2 }
+  } else if (fileName === "/features/bar") {
+    return { a: 3, b: 4 }
+  } else {
+    throw Error(`Bad filename: ${fileName}`)
+  }
+}
+
+test("loadFeatures collects everything from the directory", t => {
+  t.deepEqual(
+    loadFeatures("/features", {
+      readdirSync: () => ["foo.js", "bar.js"],
+      require: requireFeature,
+    }),
+    {
+      a: [1, 3],
+      b: [2, 4],
+    },
+  )
+})
+
+test("loadFeatures ignores .spec files", t => {
+  t.deepEqual(
+    loadFeatures("/features", {
+      readdirSync: () => ["bar.js", "bar.spec.js"],
+      require: requireFeature,
+    }),
+    {
+      a: [3],
+      b: [4],
+    },
+  )
+})
--- src/lib/loadFeatures.ts
+++ src/lib/loadFeatures.ts
@@ -1,17 +1,35 @@
-import * as fs from "fs"
 import * as path from "path"
 
-const loadFeatures = () => {
-  const features = fs
-    .readdirSync("../features")
+const loadFeatures = (
+  featuresDir: string,
+  {
+    readdirSync,
+    require,
+  }: {
+    readonly readdirSync: (dirName: string) => string[]
+    readonly require: (fileName: string) => object
+  },
+) => {
+  const features = readdirSync(path.resolve(featuresDir))
     .map(fileName => path.basename(fileName, path.extname(fileName)))
     .filter(baseName => !baseName.match(/\.spec$/))
-    .map(baseName => path.join("../features", baseName))
+    .map(baseName => path.resolve(featuresDir, baseName))
     .map(pathName => require(pathName))
 
-  return {
-    lex: features.map(feature => feature.lex).filter(Boolean),
-  }
+  const apis = [
+    ...new Set(
+      features
+        .map(Object.keys)
+        .reduce((apis, featureApi) => [...apis, ...featureApi]),
+    ),
+  ]
+
+  return Object.assign(
+    {},
+    ...apis.map(api => ({
+      [api]: features.map(feature => feature[api]).filter(Boolean),
+    })),
+  )
 }
 
 export default loadFeatures
